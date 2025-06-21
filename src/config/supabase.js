1cba9396f9698eefb8d292cf3ed0f90f00744706
yash khandal <135641850+Yash-Khandal@users.noreply.github.com>
2025-06-21 23:20:44 +0530

    Update supabase.js

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://uronuibgogisbfzwrqnn.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVyb251aWJnb2dpc2JmendycW5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAyMzk2NTYsImV4cCI6MjA2NTgxNTY1Nn0.vJRRf_ugQWMopRklFaRruFZXWhL3PitAQXQd2DTvlJQ'
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
